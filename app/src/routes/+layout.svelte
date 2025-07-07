a4435a557b4cb8de358cbcc9f67598d571d7333c
axelkrnwn <axelkurn@gmail.com>
2025-07-07 13:12:11 +0700

    Feat: Main page and shadcn + magicUI

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+    import '../app.css';
+    import '../styles/global.css';
+    import '../styles/layout.scss';
+    
     let { children } = $props();
 </script>
 
@@ -7,48 +11,3 @@
 </nav>
 
 {@render children()}
-
-<style lang="scss">
-    :global(*){
-      margin: 0;
-      padding: 0;
-    }
-
-    :global(:root){
-        --white:#FFF1D5;
-        --primary:#48A6A7;
-        --secondary:#006A71;
-        --accent:#9ACBD0;
-        --black: #1b1b1b;
-    }
-
-    @font-face {
-        font-family: 'Poppins-Regular';
-        src: url('../asset/font/Poppins-Regular.ttf');
-    }
-    @font-face {
-        font-family: 'Poppins-Light';
-        src: url('../asset/font/Poppins-Light.ttf');
-    }
-    @font-face {
-        font-family: 'Poppins-Semibold';
-        src: url('../asset/font/Poppins-SemiBold.ttf');
-    }
-    @font-face {
-        font-family: 'Poppins-Bold';
-        src: url('../asset/font/Poppins-Bold.ttf');
-    }
-
-    nav{
-        box-sizing: border-box;
-        padding: 3vh 2vw;
-        width: 100vw;
-        display: flex;
-        justify-content: right;
-        h4{
-            color: var(--secondary);
-            font-size: 1.1rem;
-            font-family: 'Poppins-Light', sans-serif;
-        }
-    }
-</style>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,8 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import Button from "@/lib/components/ui/button/button.svelte";
+  import Meteors from "@/lib/components/ui/meteors.svelte";
+
+</script>
+
+<div class="relative w-screen h-screen">
+    <Meteors number={50}/>
+    <div class="absolute left-1/2 top-1/2 flex justify-center items-center w-2/5 h-2/5 gap-5 flex-col -translate-x-1/2 -translate-y-1/2">
+        <h1 class="w-full text-center text-3xl">Learning indonesia word by Audio</h1>
+        <Button class="px-5 w-2/5 cursor-pointer hover:bg-white hover:text-[var(--secondary)] hover:border-[var(--secondary)] border-2 transition-all duration-500">Start Game</Button>
+        <p class="text-[var(--secondary)] hover:scale-125 transition-all cursor-pointer">How to <b>Play</b>?</p>
+    </div>
+</div>
+
 
 <style>
     h1{
-        font-family: 'Poppins-Bold', Serif;
+        font-family: 'Poppins-SemiBold', sans-serif;
     }
 </style>
